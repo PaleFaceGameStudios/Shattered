d9da15beb370b6156bc2f4f40bfc4f96ad4aa642
Hol1x <jkjkklh@gmail.com>
2015-05-18 21:38:37 +0200

    Script updates

---
+++ New Unity Project 1/Assets/Scripts/ISQuality.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ISQuality : IISQuality {
+	#region IISQuality implementation
+	[SerializeField]string _name;
+	[SerializeField]Sprite _icon;
+
+
+
+	ISQuality(){
+		_icon = new Sprite ();
+		_name = "common";
+	}
+
+
+
+	public string Name {
+
+		get {return _name;}
+		set {_name = value;}
+	}
+
+	public Sprite Icon {
+		get {
+			return _icon;
+		}
+		set {_icon = value;}
+	}
+
+	#endregion
+
+
+
+
+}
--- New Unity Project 1/Assets/Scripts/interface/IISObject.cs
+++ New Unity Project 1/Assets/Scripts/interface/IISObject.cs
@@ -1,18 +1,12 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public interface IISObject {
-
-	//name
-	//value - gold value
-	//icon
-	//burden
-	//qualityLevel
 	string ISName { get; set; }
 	int ISValue { get; set; }
 	Sprite ISIcon { get; set; }
 	int ISBurden { get; set; }
-
+	ISQuality ISQuality { get; set;}
 
 	//equip
 	//questItem flag
