47c3c923c61bf37951451a935a587d6c9e2e6513
Hol1x <jkjkklh@gmail.com>
2015-05-20 23:04:23 +0200

    Full update on Rotater.cs

--- New Unity Project 1/Assets/Scripts/Rotater.cs
+++ New Unity Project 1/Assets/Scripts/Rotater.cs
@@ -1,12 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
 public class Rotater : MonoBehaviour {
-	
+	public Text timer;
+	float timeLeft = 15f;
+	bool neg = false;
+
 	// Update is called once per frame
 	void Update () {
-		transform.Translate(Vector3.up * Time.deltaTime, Space.World);
-		transform.Rotate ( new Vector3 (15,15,15) * Time.deltaTime);
+		timer.text = timeLeft.ToString();
+		timeLeft -= Time.deltaTime;
+
+		if (timeLeft <= 15f) {
+			neg = false;
+		}
+		if (timeLeft > 0f && timeLeft <=15f && neg == false) {
+			transform.Translate (Vector3.down * Time.deltaTime, Space.World);
+			transform.Rotate (new Vector3 (15, 15, 15) * Time.deltaTime);
+		} else {
+			if(neg == false){
+				timeLeft = 30f;
+			}
+			neg = true;
+			transform.Translate (Vector3.up * Time.deltaTime, Space.World);
+			transform.Rotate (new Vector3 (15, 15, 15) * Time.deltaTime);
+		}
 	}
+
+
 }
 
