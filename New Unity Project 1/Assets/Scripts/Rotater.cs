c3b2dd47933be0b2e4ac8b6f548d5b4c5f715684
Hol1x <jkjkklh@gmail.com>
2015-05-20 21:05:39 +0200

    Rotater changed

--- New Unity Project 1/Assets/Scripts/Rotater.cs
+++ New Unity Project 1/Assets/Scripts/Rotater.cs
@@ -5,6 +5,7 @@ public class Rotater : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		transform.Translate(Vector3.up * Time.deltaTime, Space.World);
 		transform.Rotate ( new Vector3 (15,15,15) * Time.deltaTime);
 	}
 }
