fd8fa56ac6498d6ac596ddc117f15c191cbf820c
Rasmus Leifsgård <Leifsgard@outlook.com>
2015-05-21 00:58:41 +0200

    Script change + changes

--- New Unity Project 1/Assets/Scripts/ObjectMovementControl.cs
+++ New Unity Project 1/Assets/Scripts/ObjectMovementControl.cs
@@ -1,34 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
-public class Rotater : MonoBehaviour {
+public class ObjectMovementControl : MonoBehaviour {
 	public Text timer;
 	public bool Reverse = false;
 	public float timeLeft = 15f;
+	public int x_rotate;
+	public int y_rotate;
+	public int z_rotate;
 	float _timeLeft;
 	bool neg = false;
+	
+	
+	
 	void Start (){
-	 _timeLeft = timeLeft;
+		_timeLeft = timeLeft;
 	}
 	// Update is called once per frame
 	void Update () {
 		timer.text = timeLeft.ToString ();
 		timeLeft -= Time.deltaTime;
-
+		
 		if (Reverse) {
 			if (timeLeft <= _timeLeft) {
 				neg = false;
 			}
 			if (timeLeft > 0f && timeLeft <= _timeLeft && neg == false) {
 				transform.Translate (Vector3.down * Time.deltaTime, Space.World);
-				transform.Rotate (new Vector3 (15, 15, 15) * Time.deltaTime);
+				transform.Rotate (new Vector3 (x_rotate, y_rotate, z_rotate) * Time.deltaTime);
 			} else {
 				if (neg == false) {
 					timeLeft = _timeLeft*2;
 				}
 				neg = true;
 				transform.Translate (Vector3.up * Time.deltaTime, Space.World);
-				transform.Rotate (new Vector3 (15, 15, 15) * Time.deltaTime);
+				transform.Rotate (new Vector3 (x_rotate, y_rotate, z_rotate) * Time.deltaTime);
 			}
 		} else {
 			if (timeLeft <= _timeLeft) {
@@ -36,7 +42,7 @@ public class Rotater : MonoBehaviour {
 			}
 			if (timeLeft > 0f && timeLeft <= _timeLeft && neg == false) {
 				transform.Translate (Vector3.up * Time.deltaTime, Space.World);
-				transform.Rotate (new Vector3 (15, 15, 15) * Time.deltaTime);
+				transform.Rotate (new Vector3 (x_rotate, y_rotate, z_rotate) * Time.deltaTime);
 			} else {
 				if (neg == false) {
 					timeLeft = _timeLeft*2;
@@ -47,6 +53,5 @@ public class Rotater : MonoBehaviour {
 			}
 		}
 	}
-
+	
 }
-
