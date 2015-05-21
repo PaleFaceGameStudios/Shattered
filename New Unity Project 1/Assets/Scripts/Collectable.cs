983248cfee8228483db69fe4b9831fead0fb5193
Rasmus Leifsgård <Leifsgard@outlook.com>
2015-05-22 00:24:04 +0200

    Equipment System and a lot of other stuff.
    Needs fixes

---
+++ New Unity Project 1/Assets/Scripts/Collectable.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collectable : MonoBehaviour {
+
+	public string identifier = "";
+	public string displayName = "";
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "Player") 
+		{
+			Collect(other.gameObject);
+		}
+	}
+
+	void Collect(GameObject player)
+	{
+		player.GetComponent<Inventory>().AddEquipment(identifier);
+		Destroy(gameObject);
+	}
+}
---
+++ New Unity Project 1/Assets/Scripts/Inventory.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour {
+
+	public string equipment = "";
+	public List<string> items = new List<string>();
+	
+	public void AddEquipment(string name)
+	{
+		equipment = name;
+
+		foreach(Equipment equip in gameObject.GetComponentsInChildren<Equipment>())
+		{
+			if(equip.identifier == name)
+			{
+				foreach(Transform t in equip.transform)
+				{
+					t.gameObject.SetActive(true);
+				}
+			}
+		}
+	}
+}
