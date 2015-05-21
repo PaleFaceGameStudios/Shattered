7d06a848065cbb8e51a00c601926c1f7ef876977
Rasmus Leifsgård <Leifsgard@outlook.com>
2015-05-21 16:56:21 +0200

    I added a Menu design, but no functions for links.

---
+++ New Unity Project 1/Assets/Scripts/Menu.js
@@ -0,0 +1,12 @@
+﻿
+
+function OnMouseEnter()
+{
+GetComponent.<Renderer>().material.color = Color.grey;
+GetComponent.<AudioSource>().Play();
+}
+
+function OnMouseExit()
+{
+GetComponent.<Renderer>().material.color = Color.white;
+}
